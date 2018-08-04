2b91f3ec6239a97bdb95d351f74e3ec9b5abcf58
Jeffrey <jeffrey.q@hotmail.com>
2018-08-04 20:36:18 +1000

    Working sound stuff

--- wakeme/ViewController.swift
+++ wakeme/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 import FirebaseDatabase
+import AVFoundation
 
 class ViewController: UIViewController {
 
     var ref: DatabaseReference!
+    var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +28,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func eergeg(_ sender: Any) {
-     }
-    
 
+    
+    @IBAction func newAlarmAction(_ sender: Any) {
+        playSound()
+    }
+   
+    
+    func playSound() {
+        let path = Bundle.main.path(forResource: "Alarm Sound 1", ofType: "mp3")!
+        let url = URL(fileURLWithPath: path)
+        
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+            player.play()
+        }catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
+    
 }
 
