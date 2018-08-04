35dd9192acd0967d70c1bf2e475dca6ea451a3c4
Unknown <will.lawless96@gmail.com>
2018-08-04 20:49:07 +1000

    UI LOOKING GOOD

---
+++ wakeme/SetAlarmController.swift
@@ -0,0 +1,39 @@
+//
+//  SetAlarmController.swift
+//  wakeme
+//
+//  Created by Will Lawless on 4/8/18.
+//  Copyright © 2018 theboys. All rights reserved.
+//
+
+import UIKit
+
+class SetAlarmController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func setAlarm(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
